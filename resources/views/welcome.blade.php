aaa82691698e42b867734494d1b11ddd63800642
devsystem16 <desarrollos16@hotmail.com>
2026-03-12 01:01:16 -0500

    permisos fecha limite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
         }
     </style>
     <title>Grupo Compustar</title>
-    <script defer="defer" src="/static/js/main.1b2364a3.js"></script>
+    <script defer="defer" src="/static/js/main.f14f5a8d.js"></script>
     <link href="/static/css/main.166ee044.css" rel="stylesheet">
 </head>
 
